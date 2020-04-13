ae3fbdc493ac60a3ce62b618877bd6ce31ee589b
Duncan Bellamy <dunk@denkimushi.com>
2020-04-13 14:44:12 +0000

    pass TAG to build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Building docker image: $IMAGE_NAME"
-echo "With tag: $TAG"
+echo "With tag: $1"
 
-docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 $TAG -t "$IMAGE_NAME" --push .
+docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 "$1" -t "$IMAGE_NAME" --push .
 docker logout
