ae00a177225470f4b8e27e6ee807bdfdd9db4143
Duncan Bellamy <dunk@denkimushi.com>
2020-04-13 14:52:13 +0000

    only pass tag not image name as well

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,5 @@
 echo "Building docker image: $IMAGE_NAME"
 echo "With tag: $1"
 
-docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 "$1" -t "$IMAGE_NAME" --push .
+docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 -t "$IMAGE_NAME:$1" -t "$IMAGE_NAME" --push .
 docker logout
