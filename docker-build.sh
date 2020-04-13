f6da6768d7a89b13e6036a2a6a6fed082e2f618f
Duncan Bellamy <dunk@denkimushi.com>
2020-04-13 14:37:57 +0000

    remove quotes from TAG, echo image name and tag

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 "$TAG" -t "$IMAGE_NAME" --push .
+echo "Building docker image: $IMAGE_NAME"
+echo "With tag: $TAG"
+
+docker buildx build --platform linux/amd64,linux/386,linux/ppc64le,linux/s390x,linux/arm64,linux/arm/v7 $TAG -t "$IMAGE_NAME" --push .
 docker logout
