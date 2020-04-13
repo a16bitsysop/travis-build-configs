d29c0ecb0b354d916cabbc446744f76e7e93d813
Duncan Bellamy <dunk@denkimushi.com>
2020-04-13 14:22:35 +0000

    tidy up scripts

--- docker-prep.sh
+++ docker-prep.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker buildx create --use
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
