bd0a1918927f2d162000824c2783ff1d683aa609
Duncan Bellamy <dunk@denkimushi.com>
2020-06-20 10:11:02 +0100

    remove script now done in yml file

--- docker-prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
